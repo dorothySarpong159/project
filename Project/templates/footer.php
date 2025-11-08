b0c6ba5033c9f7f7b31a201dd0b454f36db8cb28
dorothySarpong159 <200624161@student.georgianc.on.ca>
2025-11-08 09:33:38 -0500

    Add files via upload
    templates( header and footer files)

---
+++ Project/templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; <?= date("Y") ?> FunDee Collection </p>
+</footer>
+</body>
+</html>
---
+++ Project/templates/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $pageDesc ?>">
+    <meta name="robots" content="noindex, nofollow">
+    <meta charset="utf-8">
+    <title><?php echo $pageTitle ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Delius&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+   
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header class= "global-header">
+        <nav class="main-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+</ul>
+</nav>
+</header>

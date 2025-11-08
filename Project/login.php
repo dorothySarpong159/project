4ecbe904310c18c8cefee8672403bc736dbf1177
dorothySarpong159 <200624161@student.georgianc.on.ca>
2025-11-08 09:40:50 -0500

    Add files via upload
    The log in page for users

---
+++ Project/login.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = "Login Page";
+$pageDesc ="Sign in into your account";
+require './templates/header.php';
+?>
+<main class="admin-page">
+    <section class="admin-container">
+        <div class="login-page">
+            <h1>Login</h1>
+            <form method="post" action="login.php">
+                <label for="login_email">Email</label>
+                <input type="email" id="login_email" name="login_email">
+
+                <label for="login_password">Password</label>
+                <input type="password" id="login_password" name="login_password">
+
+                <button type="submit" class="btn">Login</button>
+</form>
+<p class="register-box">Don't have an account?
+    <a href ="register.php">Register here</a>
+</p>
+</div>
+</section>
+</main>
+<?php require './templates/footer.php';?>

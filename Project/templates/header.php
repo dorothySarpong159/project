1a1b6c6713b87cfc861402be95d7b9a8f49ccc7b
dorothySarpong159 <200624161@student.georgianc.on.ca>
2025-11-09 22:08:00 -0500

    Add files via upload
    updated header- 2 fonts added and comments added

---
+++ Project/templates/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- meta data -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $pageDesc ?>">
+    <meta name="robots" content="noindex, nofollow">
+    <meta charset="utf-8">
+    <title><?php echo $pageTitle ?></title>
+
+    <!-- 2 different fonts inserted-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Delius&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+   
+    <!-- add our css file -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+    <!--global header-->
+    <header class= "global-header">
+
+    <!-- navigation links -->
+        <nav class="main-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+</ul>
+</nav>
+</header>
